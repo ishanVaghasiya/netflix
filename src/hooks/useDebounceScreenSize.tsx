3d90f93dcf73e9b669e75b41d7c30fba623cf615
ishanVaghasiya <98661936+ishanVaghasiya@users.noreply.github.com>
2023-11-07 22:47:19 +0530

    add details  or commnet in useDebounceScreenSize.tsx

--- src/hooks/useDebounceScreenSize.tsx
+++ src/hooks/useDebounceScreenSize.tsx
@@ -2,13 +2,17 @@
 import React, { useState, useEffect, useCallback } from "react";
 import debounce from "lodash/debounce";
 
+// Interface for defining the screen dimension object
 interface Dimension {
   height: number;
   width: number;
 }
 
+// Custom hook for obtaining debounced screen dimensions
 export const useDebounceScreenSize = (time: number = 0) => {
   const hasWindow = typeof window !== "undefined";
+
+   // Function to get the current screen dimensions
   const getDimension = () => {
     if (hasWindow) {
       return {
@@ -17,10 +21,13 @@ export const useDebounceScreenSize = (time: number = 0) => {
       };
     }
   };
+
+   // State to store the screen dimensions
   const [dimension, setDimension] = useState<Dimension | undefined>(
     getDimension()
   );
 
+  // Callback function to handle screen resizing with debounce
   const handleResize = useCallback(() => {
     const debouncedHandleResize = debounce(() => {
       setDimension(getDimension());
@@ -30,6 +37,8 @@ export const useDebounceScreenSize = (time: number = 0) => {
       window.removeEventListener("resize", debouncedHandleResize);
     };
   }, []);
+
+  // useEffect to trigger the handleResize callback on component mount
   useEffect(() => handleResize(), []);
   return { width: dimension?.width, height: dimension?.height };
 };
